4e0187cc688795d8c192e4aeb0774c41f5b78a31
Dimitri Müller <dimitrimuller@outlook.com>
2023-12-28 22:07:56 -0300

    class cleanup and services creation

---
+++ Consumer1/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer1.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string? first_name { get; set; }
+        public string? last_name { get; set; }
+        public string? sex { get; set; }
+        public DateTime birth_date { get; set; }
+         
+        public static Customer GetCustomers()
+        {
+            return new Customer()
+            {
+                id = 1,
+                first_name = "Joe",
+                last_name = "Doe",
+                sex = "Male",
+                birth_date = new DateTime(1980, 2, 2)
+            };
+        }
+    }
+}
---
+++ Consumer1/Services/MySQLConsumer.cs
@@ -0,0 +1,46 @@
+﻿using MongoDB.Driver.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Xml.Linq;
+using Consumer1.Models;
+
+namespace Consumer1.Services
+{
+    public class MySQLConsumer
+    {
+        public void getCustomersMySQL()
+        {
+            List<Customer> customers = new List<Customer>();
+            string? connetionString = null;
+            string server = "sql5.freesqldatabase.com";
+            string database = "sql5673207";
+            string username = "sql5673207";
+            string password = "8PH51R8Euv";
+
+            MySqlConnection cnn;
+            connetionString = "Server=" + server + ";Database=" + database + ";Uid=" + username + ";Pwd=" + password + ";";
+            cnn = new MySqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                MySqlCommand com = cnn.CreateCommand();
+                com.CommandText = "SELECT * FROM customer";
+                MySqlDataReader reader = com.ExecuteReader();
+                Console.WriteLine($"{reader.GetName(0),-4} {reader.GetName(1),-10} {reader.GetName(2),10} {reader.GetName(3),10} {reader.GetName(4),10}");
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader.GetInt32(0),-4} {reader.GetString(1),-10} {reader.GetString(2),10} {reader.GetString(3),10} {reader.GetMySqlDateTime(4),10}");
+                }
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
