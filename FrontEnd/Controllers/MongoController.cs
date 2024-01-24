e901a6ac93a093a779b523b1355e3ebe212c69f1
Dimitri Müller <dimitrimuller@outlook.com>
2024-01-24 17:26:14 -0300

    segregating database connections

--- FrontEnd/Controllers/MongoController.cs
+++ FrontEnd/Controllers/MongoController.cs
@@ -7,13 +7,13 @@ namespace FrontEnd.Controllers
     {
         public List<Customer> GetCustomersMongo()
         {
-            var mongoUri = "mongodb+srv://admin:admin@cluster1.j4jqugk.mongodb.net/";
-            MongoClient client;
-            IMongoCollection<Customer> collection;
-
             try
             {
-                client = new MongoClient(mongoUri);
+                var databaseController = new DatabaseController();
+                var collection = databaseController.GetCollection<Customer>();
+
+                var customers = collection.Find(_ => true).ToList();
+                return customers;
             }
             catch (Exception e)
             {
@@ -26,64 +26,34 @@ namespace FrontEnd.Controllers
 
                 return new List<Customer>();
             }
-
-            var dbName = "Cluster1DB";
-            var collectionName = "customer";
-
-            collection = client.GetDatabase(dbName)
-               .GetCollection<Customer>(collectionName);
-            var customers = collection.Find(_ => true).ToList();
-
-            return customers;
         }
 
 		public async Task AddCustomerMongoDB(List<Customer> customers)
 		{
-			var mongoUri = "mongodb+srv://admin:admin@cluster1.j4jqugk.mongodb.net/";
-			MongoClient client;
-			IMongoCollection<Customer> collection;
+            try
+            {
+                var databaseController = new DatabaseController();
+                var collection = databaseController.GetCollection<Customer>();
 
-			try
-			{
-				client = new MongoClient(mongoUri);
-			}
-			catch (Exception e)
+                foreach (var customer in customers)
+                {
+                    customer.status = 2;
+                    customer.updated_at = DateTime.Now;
+
+                    var filter = Builders<Customer>.Filter.Eq(c => c.id, customer.id);
+                    var updateOptions = new ReplaceOptions { IsUpsert = true };
+
+                    await collection.ReplaceOneAsync(filter, customer, updateOptions);
+                }
+
+                Console.WriteLine($"Successfully inserted {customers.Count()} new customers in MongoDB.");
+            }
+            catch (Exception e)
 			{
 				Console.WriteLine("There was a problem connecting to your " +
 					"Atlas cluster. Check that the URI includes a valid " +
 					"username and password, and that your IP address is " +
 					$"in the Access List. Message: {e.Message}");
-				Console.WriteLine(e);
-				Console.WriteLine();
-
-				return;
-			}
-
-			var dbName = "Cluster1DB";
-			var collectionName = "customer";
-
-			collection = client.GetDatabase(dbName)
-			   .GetCollection<Customer>(collectionName);
-
-			try
-			{
-				foreach (var customer in customers)
-				{
-					customer.status = 2;
-					customer.updated_at = DateTime.Now;
-
-					var filter = Builders<Customer>.Filter.Eq(c => c.id, customer.id);
-					var updateOptions = new ReplaceOptions { IsUpsert = true }; // This will insert if the document doesn't exist
-
-					await collection.ReplaceOneAsync(filter, customer, updateOptions);
-				}
-
-				Console.WriteLine($"Successfully inserted {customers.Count()} new customers in MongoDB.");
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine($"Something went wrong trying to insert the new documents." +
-					$" Message: {e.Message}");
 				Console.WriteLine(e);
 				Console.WriteLine();
 
--- FrontEnd/Controllers/MySqlController.cs
+++ FrontEnd/Controllers/MySqlController.cs
@@ -4,158 +4,113 @@ using MySql.Data.MySqlClient;
 namespace FrontEnd.Controllers
 {
     public class MySqlController
-	{
-		public List<Customer> GetCustomersMySql()
-		{
-			List<Customer> customers = new List<Customer>();
-            string server = "sql5.freesqldatabase.com";
-			string database = "sql5673207";
-			string username = "sql5673207";
-			string password = "8PH51R8Euv";
-			MySqlCommand com = new MySqlCommand();
-			MySqlDataReader dr;
-			MySqlConnection con = new MySqlConnection();
+    {
+        private readonly DatabaseController _databaseController;
 
-            string? connetionString = "Server=" + server + ";Database=" + database + ";Uid=" + username + ";Pwd=" + password + ";";
-            con.ConnectionString = connetionString;
+        public MySqlController()
+        {
+            _databaseController = new DatabaseController();
+        }
 
-			try
-			{
-				con.Open();
-				com.Connection = con;
-				com.CommandText = "SELECT id,first_name,last_name,sex,birth_date,status,updated_at FROM customer";
-				dr = com.ExecuteReader();
+        public List<Customer> GetCustomersMySql()
+        {
+            List<Customer> customers = new List<Customer>();
+            using (MySqlConnection con = _databaseController.GetConnection())
+            {
+                using (MySqlCommand com = new MySqlCommand("SELECT id,first_name,last_name,sex,birth_date,status,updated_at FROM customer", con))
+                {
+                    con.Open();
+                    using (MySqlDataReader dr = com.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            customers.Add(new Customer()
+                            {
+                                id = (int)dr["id"],
+                                first_name = dr["first_name"].ToString(),
+                                last_name = dr["last_name"].ToString(),
+                                sex = dr["sex"].ToString(),
+                                birth_date = (DateTime)dr["birth_date"],
+                                status = (int)dr["status"],
+                                updated_at = (DateTime)dr["updated_at"]
+                            });
+                        }
+                        con.Close();
 
-				while (dr.Read())
-				{
-					customers.Add(new Customer()
-					{
-						id = (int)dr["id"],
-						first_name = dr["first_name"].ToString(),
-						last_name = dr["last_name"].ToString(),
-						sex = dr["sex"].ToString(),
-						birth_date = (DateTime)dr["birth_date"],
-						status = (int)dr["status"],
-						updated_at = (DateTime)dr["updated_at"]
-					});
-				}
-				con.Close();
-
-				return customers;
-			}
-			catch (Exception)
-			{
-                throw;
+                        return customers;
+                    }
+                }
             }
-		}
+        }
 
         public List<Customer> SyncCustomersMySql()
         {
             List<Customer> customers = new List<Customer>();
-            string server = "sql5.freesqldatabase.com";
-            string database = "sql5673207";
-            string username = "sql5673207";
-            string password = "8PH51R8Euv";
-            MySqlCommand com = new MySqlCommand();
-            MySqlDataReader dr;
-            MySqlConnection con = new MySqlConnection();
-
-            string? connetionString = "Server=" + server + ";Database=" + database + ";Uid=" + username + ";Pwd=" + password + ";";
-            con.ConnectionString = connetionString;
-
-            try
+            using (MySqlConnection con = _databaseController.GetConnection())
             {
-                con.Open();
-                com.Connection = con;
-                com.CommandText = "SELECT id,first_name,last_name,sex,birth_date,status,updated_at FROM customer where status = 1";
-                dr = com.ExecuteReader();
-
-                while (dr.Read())
+                using (MySqlCommand com = new MySqlCommand("SELECT id,first_name,last_name,sex,birth_date,status,updated_at FROM customer WHERE status = 1", con))
                 {
-                    customers.Add(new Customer()
+                    con.Open();
+                    using (MySqlDataReader dr = com.ExecuteReader())
                     {
-                        id = (int)dr["id"],
-                        first_name = dr["first_name"].ToString(),
-                        last_name = dr["last_name"].ToString(),
-                        sex = dr["sex"].ToString(),
-                        birth_date = (DateTime)dr["birth_date"],
-                        status = (int)dr["status"],
-                        updated_at = (DateTime)dr["updated_at"]
-                    });
+                        while (dr.Read())
+                        {
+                            customers.Add(new Customer()
+                            {
+                                id = (int)dr["id"],
+                                first_name = dr["first_name"].ToString(),
+                                last_name = dr["last_name"].ToString(),
+                                sex = dr["sex"].ToString(),
+                                birth_date = (DateTime)dr["birth_date"],
+                                status = (int)dr["status"],
+                                updated_at = (DateTime)dr["updated_at"]
+                            });
+                        }
+                    }
                 }
-                con.Close();
+            }
 
-                return customers;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            return customers;
         }
 
         public async Task UpdateMySqlCustomers(List<Customer> customers)
-		{
-            string server = "sql5.freesqldatabase.com";
-            string database = "sql5673207";
-            string username = "sql5673207";
-            string password = "8PH51R8Euv";
-            MySqlCommand com = new MySqlCommand();
-            MySqlConnection con = new MySqlConnection();
+        {
+            using (MySqlConnection con = _databaseController.GetConnection())
+            {
+                await con.OpenAsync();
 
-            string? connetionString = "Server=" + server + ";Database=" + database + ";Uid=" + username + ";Pwd=" + password + ";";
-            con.ConnectionString = connetionString;
-			foreach (var customer in customers)
-			{
-                try
+                foreach (var customer in customers)
                 {
-                    con.Open();
-                    com = con.CreateCommand();
-                    com.CommandText = "UPDATE customer set status = 2 where id = @id";
-                    com.Parameters.AddWithValue("@id", customer.id);
+                    using (MySqlCommand com = con.CreateCommand())
+                    {
+                        com.CommandText = "UPDATE customer SET status = 2 WHERE id = @id";
+                        com.Parameters.AddWithValue("@id", customer.id);
 
-                    await com.ExecuteNonQueryAsync();
-
-                    con.Close();
+                        await com.ExecuteNonQueryAsync();
+                    }
                 }
-                catch (Exception)
-                {
-                    throw;
-                }
-            }            
+            }
         }
 
         public async Task InsertCustomerMySql(Customer customer)
-		{
-            string server = "sql5.freesqldatabase.com";
-			string database = "sql5673207";
-			string username = "sql5673207";
-			string password = "8PH51R8Euv";
-			MySqlCommand com = new MySqlCommand();
-			MySqlConnection con = new MySqlConnection();
+        {
+            using (MySqlConnection con = _databaseController.GetConnection())
+            {
+                await con.OpenAsync();
 
-            string? connetionString = "Server=" + server + ";Database=" + database + ";Uid=" + username + ";Pwd=" + password + ";";
-            con.ConnectionString = connetionString;
+                using (MySqlCommand com = con.CreateCommand())
+                {
+                    com.CommandText = "INSERT INTO customer (first_name, last_name, sex, birth_date, status, updated_at) VALUES (@first_name, @last_name, @sex, @birth_date, @status, @updated_at)";
+                    com.Parameters.AddWithValue("@first_name", customer.first_name);
+                    com.Parameters.AddWithValue("@last_name", customer.last_name);
+                    com.Parameters.AddWithValue("@sex", customer.sex);
+                    com.Parameters.AddWithValue("@birth_date", customer.birth_date);
+                    com.Parameters.AddWithValue("@status", customer.status);
+                    com.Parameters.AddWithValue("@updated_at", customer.updated_at);
 
-			try
-			{
-				con.Open();
-				com = con.CreateCommand();
-				com.CommandText = "INSERT INTO customer (first_name, last_name, sex, birth_date, status, updated_at) VALUES (@first_name, @last_name, @sex, @birth_date, @status, @updated_at)";
-				com.Parameters.AddWithValue("@first_name", customer.first_name);
-				com.Parameters.AddWithValue("@last_name", customer.last_name);
-				com.Parameters.AddWithValue("@sex", customer.sex);
-				com.Parameters.AddWithValue("@birth_date", customer.birth_date);
-				com.Parameters.AddWithValue("@status", customer.status);
-				com.Parameters.AddWithValue("@updated_at", customer.updated_at);
-
-				await com.ExecuteNonQueryAsync();
-
-				con.Close();
-			}
-			catch (Exception)
-			{
-				throw;
-			}
-		}
-	}
+                    await com.ExecuteNonQueryAsync();
+                }
+            }
+        }
+    }
 }
