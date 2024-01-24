a3a5918a91343fdcd0567a0970209bc91b73e504
Dimitri Müller <dimitrimuller@outlook.com>
2024-01-23 21:41:02 -0300

    creating customer controller to build the customer from form data

---
+++ FrontEnd/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using FrontEnd.Models;
+
+namespace FrontEnd.Controllers
+{
+    public class CustomerController
+    {
+        public Customer BuildCustomer(IFormCollection form)
+        {
+            Customer customer = new Customer();
+            customer.first_name = form["firstName"];
+            customer.last_name = form["lastName"];
+            customer.sex = form["sex"];
+            customer.birth_date = DateTime.Parse(form["birthDate"]);
+            customer.status = 1;
+            customer.updated_at = DateTime.Now;
+            return customer;
+        }
+    }
+}
